cdeca86c4eb85438d51d52d5a70fd644903dec85
crava-logico-srl <p.cravanzola@logico.srl>
2023-01-11 11:48:39 +0100

    userouter client

--- src/pages/patients/[patientId]/index.tsx
+++ src/pages/patients/[patientId]/index.tsx
@@ -1,13 +1,18 @@
+import { useRouter } from "next/router";
 import { SplittedPage } from "../../../components/layout/splittedPage";
 import { Patient } from "../../../components/patients/patient";
 
 
-const PatientsPage: PageComponent = ({ params }) => {
-    const { patientId } = params as { patientId: string }
+const PatientsPage: PageComponent = ({ }) => {
+
+    const { query } = useRouter()
+
+
+    const { patientId } = query as { patientId: string }
 
     const Comp = () => <>
         <h1>Paziente</h1>
-        <Patient patientId={params.patientId} />
+        <Patient patientId={patientId} />
     </>
 
     return (<SplittedPage Left={Comp} />)
