3926236651dd3ec1e770a1018503164ff258db4b
crava-logico-srl <p.cravanzola@logico.srl>
2023-09-04 13:14:09 +0200

    online pubmed search

--- src/@types/model/pubmed/IPubMedSearchResult.d.ts
+++ src/@types/model/pubmed/IPubMedSearchResult.d.ts
@@ -8,6 +8,12 @@ type IPubMedSummaryResultResponse = {
     result: IPubMedSummaryResult
 }
 
+type IPubMedDetailResultResponse = any;
+// {
+//     header: IPubMedResultHeader,
+//     result: IPubMedDetailResult
+// }
+
 type IPubMedResponse = { search: IPubMedSearchResult } & { summary: IPubMedSummaryResult };
 
 type IPubMedResultHeader = {
@@ -31,6 +37,252 @@ type IPubMedSearchResult = {
 type IPubMedSummaryResult = {
     uids: string[],
     [key: string]: IPubMedSummary
+}
+
+type IPubMedDetail = {
+    PubmedArticleSet: {
+        PubmedArticle: {
+            MedlineCitation: {
+                _attributes: {
+                    Status: string,
+                    Owner: string
+                },
+                DateCompleted: {
+                    Year: {
+                        _text: string
+                    },
+                    Month: {
+                        _text: string
+                    },
+                    Day: {
+                        _text: string
+                    }
+                },
+                DateRevised: {
+                    Year: {
+                        _text: string
+                    },
+                    Month: {
+                        _text: string
+                    },
+                    Day: {
+                        _text: string
+                    }
+                },
+                Article: {
+                    Journal: {
+                        ISSN: {
+                            _attributes: {
+                                IssnType: string
+                            },
+                            _text: string
+                        },
+                        JournalIssue: {
+                            _attributes: {
+                                CitedMedium: string
+                            },
+                            Volume: {
+                                _text: string
+                            },
+                            Issue: {
+                                _text: string
+                            },
+                            PubDate: {
+                                Year: {
+                                    _text: string
+                                },
+                                Month: {
+                                    _text: string
+                                }
+                            }
+                        },
+                        Title: {
+                            _text: string
+                        },
+                        ISOAbbreviation: {
+                            _text: string
+                        }
+                    },
+                    ArticleTitle: {
+                        _text: string
+                    },
+                    Pagination: any,
+                    Abstract?: {
+                        AbstractText: {
+                            _text: string
+                        }
+                    },
+                    AuthorList: {
+                        Author: {
+                            _attributes: {
+                                ValidYN: string
+                            },
+                            LastName: {
+                                _text: string
+                            },
+                            ForeName: {
+                                _text: string
+                            },
+                            Initials: {
+                                _text: string
+                            },
+                            AffiliationInfo: {
+                                Affiliation: {
+                                    _text: string
+                                }
+                            }
+                        }[] | {
+                            _attributes: {
+                                ValidYN: string
+                            },
+                            LastName: {
+                                _text: string
+                            },
+                            ForeName: {
+                                _text: string
+                            },
+                            Initials: {
+                                _text: string
+                            },
+                            AffiliationInfo: {
+                                Affiliation: {
+                                    _text: string
+                                }
+                            }
+                        }
+                    },
+                    Language: {
+                        _text: string
+                    },
+                    PublicationTypeList: {
+                        PublicationType: {
+                            _attributes: {
+                                UI: string
+                            },
+                            _text: string
+                        }[]
+                    }
+                },
+                MedlineJournalInfo: {
+                    Country: {
+                        _text: string
+                    },
+                    MedlineTA: {
+                        _text: string
+                    },
+                    NlmUniqueID: {
+                        _text: 0425076
+                    },
+                    ISSNLinking: {
+                        _text: string
+                    }
+                },
+                ChemicalList: {
+                    Chemical: {
+                        RegistryNumber: {
+                            _text: 0
+                        },
+                        NameOfSubstance: {
+                            _attributes: {
+                                UI: string
+                            },
+                            _text: string
+                        }
+                    }
+                },
+                CitationSubset: {
+                    _text: string
+                },
+                MeshHeadingList: {
+                    MeshHeading: {
+                        DescriptorName: {
+                            _attributes: {
+                                UI: string,
+                                MajorTopicYN: string
+                            },
+                            _text: string
+                        },
+                        QualifierName: {
+                            _attributes: {
+                                UI: string,
+                                MajorTopicYN: string
+                            },
+                            _text: string
+                        }
+                    }[]
+                }
+            },
+            PubmedData: {
+                History: {
+                    PubMedPubDate: [{
+                        _attributes: {
+                            PubStatus: string
+                        },
+                        Year: {
+                            _text: string
+                        },
+                        Month: {
+                            _text: string
+                        },
+                        Day: {
+                            _text: string
+                        }
+                    }, {
+                        _attributes: {
+                            PubStatus: string
+                        },
+                        Year: {
+                            _text: string
+                        },
+                        Month: {
+                            _text: string
+                        },
+                        Day: {
+                            _text: string
+                        },
+                        Hour: {
+                            _text: string
+                        },
+                        Minute: {
+                            _text: string
+                        }
+                    }, {
+                        _attributes: {
+                            PubStatus: string
+                        },
+                        Year: {
+                            _text: string
+                        },
+                        Month: {
+                            _text: string
+                        },
+                        Day: {
+                            _text: string
+                        },
+                        Hour: {
+                            _text: string
+                        },
+                        Minute: {
+                            _text: string
+                        }
+                    }
+                    ]
+                },
+                PublicationStatus: {
+                    _text: string
+                },
+                ArticleIdList: {
+                    ArticleId: {
+                        _attributes: {
+                            IdType: string
+                        },
+                        _text: string
+                    }
+                    []
+                }
+            }
+        }
+    }
 }
 
 type IPubMedSummary = {
